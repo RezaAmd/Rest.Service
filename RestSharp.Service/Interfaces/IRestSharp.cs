2b2646680539fb0aa3736a098e1c139e6bf53b32
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2021-11-25 12:59:50 +0330

    Initialize

---
+++ RestSharp.Service/Interfaces/IRestSharp.cs
@@ -0,0 +1,10 @@
+﻿using RestSharp.Service.Models;
+using System.Threading.Tasks;
+
+namespace RestSharp.Service.Interfaces
+{
+    internal interface IRestSharp
+    {
+        Task<IRestResponse> RequestAsync(string url, Method method = Method.GET, RestConfig configs = default);
+    }
+}
---
+++ RestSharp.Service/Models/RestConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace RestSharp.Service.Models
+{
+    public class RestConfig
+    {
+        #region Constructor
+        public RestConfig(string authorization = default, object body = default, Dictionary<string, string> headers = default)
+        {
+            Headers = new Dictionary<string, string>();
+            Parameters = new Dictionary<string, string>();
+            Authorization = authorization;
+            Body = body;
+            Headers = headers;
+        }
+        #endregion
+        public string Authorization { get; set; }
+        public object Body { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
+        public Dictionary<string, string> Parameters { get; set; }
+    }
+}
